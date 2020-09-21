0e4ab562614cb8dff75a827242f7b3b64f7e1f6e
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-21 12:51:08 +0200

    Add PostAdd View

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -8,11 +8,92 @@ import clsx from 'clsx';
 
 import styles from './PostAdd.module.scss';
 
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+
+
 const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>PostAdd</h2>
-    {children}
-  </div>
+  <Paper className={styles.root}>
+    <h2>Add new post</h2>
+    <form className={clsx(className, styles.form)}>
+      <TextField
+        id="title"
+        label="Title"
+        variant="outlined"
+        InputProps={{
+          minLength: 10,
+        }}
+        required
+        fullWidth
+        className={styles.title}
+      />
+      <TextField
+        id="content"
+        label="Content"
+        multiline
+        variant="outlined"
+        InputProps={{
+          minLength: 20,
+        }}
+        required
+        fullWidth
+        className={styles.description}
+      />
+
+
+
+      <TextField
+        id="price"
+        label="Price"
+        variant="outlined"
+        type="number"
+        inputProps={{
+          min: 0,
+        }}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        }}
+        className={styles.price}
+      />
+      <TextField
+        id="phone"
+        label="Phone number"
+        variant="outlined"
+        type="tel"
+        className={styles.phone}
+      />
+      <TextField
+        id="location"
+        label="Location"
+        variant="outlined"
+        className={styles.location}
+      />
+
+      <TextField
+        id="file"
+        variant="outlined"
+        type="file"
+        fullWidth
+        className={styles.file}
+      />
+      <Button
+        variant="outlined"
+        color="primary"
+        size="large"
+        className={styles.button}
+      >
+        Submit
+      </Button>
+
+    </form>
+  </Paper>
+
+
+
+
 );
 
 Component.propTypes = {
