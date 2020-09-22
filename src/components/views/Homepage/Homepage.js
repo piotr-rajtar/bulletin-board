188607bc05f093620cad252aec1864ddb622956c
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-22 18:51:45 +0200

    Add Homepage view with sample posts

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -3,35 +3,75 @@ import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAll } from '../../../redux/postsRedux';
 
 import styles from './Homepage.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>Homepage</h2>
-    {children}
-  </div>
-);
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import {NavLink} from 'react-router-dom';
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+class Component extends React.Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    posts: PropTypes.array,
+  }
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+  render() {
+    const {className, children, posts} = this.props;
+
+    return(
+      <Paper className={clsx(className, styles.root)}>
+        <div >
+          <h2>Cards</h2>
+          <p>Please, find something you can bullet-in</p>
+          <div className={styles.cardContainer}>
+            {posts.map(post => (
+              <Card
+                variant="outlined"
+                key={post.id}
+                className={styles.card}
+              >
+                <CardContent>{post.title}</CardContent>
+                <CardActions>
+                  <Button
+                    variant="outlined"
+                    size="small"
+                    color="primary"
+                    className={styles.button}
+                    component={NavLink}
+                    exact to={`/post/${post.id}`}
+                  >
+                  Find out more
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
+          {children}
+        </div>
+      </Paper>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  posts: getAll(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Homepage,
-  // Container as Homepage,
+  //Component as Homepage,
+  Container as Homepage,
   Component as HomepageComponent,
 };
--- src/components/views/Homepage/Homepage.test.js
+++ src/components/views/Homepage/Homepage.test.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HomepageComponent } from './Homepage';
 
+const posts = [
+  {
+    id: 1,
+    title: 'Title',
+    content: 'Content',
+    publicationDate: 'Publication date',
+    lastUpdate: 'Last update date',
+    email: 'Email',
+    status: 'Status',
+    photo: 'Photo',
+    price: 0,
+    phone: 123456789,
+    location: 'Location',
+  },
+];
+
 describe('Component Homepage', () => {
   it('should render without crashing', () => {
-    const component = shallow(<HomepageComponent />);
+    const component = shallow(<HomepageComponent posts={posts} />);
     expect(component).toBeTruthy();
   });
 });
