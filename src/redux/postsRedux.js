d4ef2d4406412099193c26e86c4f57ba6d991ba9
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-25 19:37:47 +0200

    Add new actions and reducers in postsRedux file

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -19,11 +19,15 @@ const createActionName = name => `app/${reducerName}/${name}`;
 const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
+const ADD_POST = createActionName('ADD_POST');
+const UPDATE_POST = createActionName('UPDATE_POST');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+export const addPost = payload => ({ payload, type: ADD_POST });
+export const updatePost = payload => ({ payload, type: UPDATE_POST });
 
 /* thunk creators */
 
@@ -56,6 +60,23 @@ export const reducer = (statePart = [], action = {}) => {
           active: false,
           error: action.payload,
         },
+      };
+    }
+    case ADD_POST: {
+      return {
+        ...statePart,
+        data: [
+          ...statePart.data,
+          action.payload
+        ]
+      };
+    }
+    case UPDATE_POST: {
+      return {
+        ...statePart,
+        data: [
+          ...statePart.data.map(post => post.id === action.payload.id?  action.payload  : post )
+        ]
       };
     }
     default:
