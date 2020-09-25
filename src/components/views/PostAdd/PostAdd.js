7c8684f10638f3fbe16b1f4ccc633f7eb659585f
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-25 23:14:14 +0200

    Amend AddPost view - without file uploading

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import clsx from 'clsx';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { addPost } from '../../../redux/postsRedux';
 
 import styles from './PostAdd.module.scss';
 
@@ -13,95 +11,212 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
 
-const Component = ({className, children}) => (
-  <Paper className={styles.root}>
-    <h2>Add new post</h2>
-    <form className={clsx(className, styles.form)}>
-      <TextField
-        id="title"
-        label="Title"
-        variant="outlined"
-        InputProps={{
-          minLength: 10,
-        }}
-        required
-        fullWidth
-        className={styles.formFieldFullWidth}
-      />
-      <TextField
-        id="content"
-        label="Content"
-        multiline
-        variant="outlined"
-        InputProps={{
-          minLength: 20,
-        }}
-        required
-        fullWidth
-        className={styles.formFieldFullWidth}
-      />
-      <TextField
-        id="price"
-        label="Price"
-        variant="outlined"
-        type="number"
-        inputProps={{
-          min: 0,
-        }}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-        className={styles.formFieldPartialWidth}
-      />
-      <TextField
-        id="phone"
-        label="Phone number"
-        variant="outlined"
-        type="tel"
-        className={styles.formFieldPartialWidth}
-      />
-      <TextField
-        id="location"
-        label="Location"
-        variant="outlined"
-        className={styles.formFieldPartialWidth}
-      />
-      <TextField
-        id="file"
-        variant="outlined"
-        type="file"
-        fullWidth
-        className={styles.formFieldFullWidth}
-      />
-      <Button
-        variant="outlined"
-        color="primary"
-        size="large"
-        className={styles.button}
-      >
-        Submit
-      </Button>
-    </form>
-  </Paper>
-);
+import ImageUploader from 'react-images-upload';
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-};
+import { v4 as uuidv4 } from 'uuid';
+
+class Component extends React.Component {
+
+  state = {
+    postData: {
+      id: '',
+      title: '',
+      content: '',
+      publicationDate: '',
+      lastUpdate: '',
+      email: '',
+      status: '',
+      photo: null,
+      price: '',
+      phone: '',
+      location: ''
+    },
+    loading: {
+      error: false,
+    },
+  }
+
+  static propTypes = {
+    addPost: PropTypes.func,
+  }
+
+  handleChange = (event) => {
+    const { postData } = this.state;
+    const {value, id } = event.target;
+
+    this.setState({
+      postData: {
+        ...postData,
+        [id]: value
+      },
+    });
+  }
+
+  createDate = () => {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+
+    const formatedDate = `${day}.${month.toString().padStart(2, '0')}.${year}`;
+    return formatedDate;
+  }
+
+  fillNoVisibleParameters = () => {
+    const { postData } = this.state;
+    const date = this.createDate();
+    const id = uuidv4();
+
+    this.setState({
+      postData: {
+        ...postData,
+        id: id,
+        publicationDate: date,
+        lastUpdate: date,
+        status: 'published',
+      },
+    });
+  }
+
+  setPhoto = (file) => {
+    const { postData } = this.state;
+
+    console.log(file);
+
+    if(file) this.setState({
+      postData: {
+        ...postData,
+        photo: file[0]
+      }
+    });
+  }
+
+  submitForm = (event) => {
+    const { postData } = this.state;
+    const { addPost } = this.props;
+    event.preventDefault();
+
+    let error = null;
+
+    if (!postData.title.length || !postData.content.length || !postData.email.length) error ='You cannot leave title, content and email firlds empty';
+    else if(postData.title.length < 10 || postData.content.length < 20) error ="Too short, title cannot has less than 10 character, content less than 20";
+
+    if(!error) {
+      addPost(postData);
+    }
+  }
+
+  render() {
+
+    return(
+      <Paper className={styles.root}>
+        <h2>Add new post</h2>
+        <form
+          className={styles.form}
+          onSubmit={this.submitForm}
+        >
+          <TextField
+            id="title"
+            label="Title"
+            variant="outlined"
+            InputProps={{
+              minLength: 10,
+            }}
+            required
+            fullWidth
+            className={styles.formFieldFullWidth}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="content"
+            label="Content"
+            multiline
+            variant="outlined"
+            InputProps={{
+              minLength: 20,
+            }}
+            required
+            fullWidth
+            className={styles.formFieldFullWidth}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="price"
+            label="Price"
+            variant="outlined"
+            type="number"
+            inputProps={{
+              min: 0,
+            }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            }}
+            className={styles.formFieldPartialWidth}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="phone"
+            label="Phone number"
+            variant="outlined"
+            type="tel"
+            className={styles.formFieldPartialWidth}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="location"
+            label="Location"
+            variant="outlined"
+            className={styles.formFieldPartialWidth}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            type="email"
+            required
+            fullWidth
+            className={styles.formFieldFullWidth}
+            onChange={this.handleChange}
+          />
+          <ImageUploader
+            withIcon={true}
+            buttonText='Choose image'
+            imgExtension={['.jpg', '.gif', '.png', '.gif']}
+            maxFileSize={5242880}
+            withPreview={true}
+            onChange={this.setPhoto}
+            singleImage={true}
+          />
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            className={styles.button}
+            type="submit"
+            onClick={this.fillNoVisibleParameters}
+          >
+            Submit
+          </Button>
+        </form>
+      </Paper>
+    );
+  }
+}
+
 
 // const mapStateToProps = state => ({
 //   someProp: reduxSelector(state),
 // });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+   addPost: data => dispatch(addPost(data)),
+ });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(null, mapDispatchToProps)(Component);
 
 export {
-  Component as PostAdd,
-  // Container as PostAdd,
+  //Component as PostAdd,
+  Container as PostAdd,
   Component as PostAddComponent,
 };
