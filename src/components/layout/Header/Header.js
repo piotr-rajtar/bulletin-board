4cc139319194eb0e5b534e914e4d757a251ce41d
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-27 13:50:42 +0200

    Connect header to redux

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,73 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { connect } from 'react-redux';
+import { getAllUsers } from '../../../redux/usersRedux';
 
 import styles from './Header.module.scss';
 
 import {NavLink, Link} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
+
 class Component extends React.Component {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
+    users: PropTypes.array,
   }
 
   state = {
-    authorizationStatus: 'not logged',
+    permission: 'not authorized',
   }
 
   handleChange = event => {
     this.setState({
-      authorizationStatus: event.target.value,
+      permission: event.target.value,
     });
   };
 
   render() {
-    const {} = this.props;
+    const { users } = this.props;
+    const { permission } = this.state;
 
     return (
       <header className={styles.root}>
         <h2 className={styles.title}><Link to={'/'} className={styles.link}>Bulletin Board</Link></h2>
 
         <select value={this.state.authorizationStatus} onChange={this.handleChange} className={styles.header_item}>
-          <option value="logged">logged</option>
-          <option value="not logged">not logged</option>
-          <option value="admin">admin</option>
+          {users.map(user => {
+            return(
+              <option key={user.id} value={user.permission}>{user.name}</option>
+            );
+          })}
         </select>
 
-        {this.state.authorizationStatus !== 'not logged'
-          ?
-          <nav>
-            <Button className={styles.link} component={NavLink} exact to={`/`} activeClassName='active'>HomePage</Button>
-            <Button className={styles.link} component={NavLink} exact to={`/post/myposts`} activeClassName='active'>My Posts</Button>
-            <Button className={styles.link} component={NavLink} exact to={`/post/add`} activeClassName='active'>Add Post</Button>
-            <Button className={styles.link} component={NavLink} exact to={`/`}>Log out</Button>
-          </nav>
-          :
-          <nav>
-            <Button><a href="https://google.com" className={styles.link}>Log in</a></Button>
-          </nav>
+        {permission === 'not authorized'
+          ? this.userNotAuthorized()
+          : this.userAuthorized()
         }
+
       </header>
+    );
+  }
+
+  userAuthorized() {
+    return(
+      <nav>
+        <Button className={styles.link} component={NavLink} exact to={`/`} activeClassName='active'>HomePage</Button>
+        <Button className={styles.link} component={NavLink} exact to={`/post/myposts`} activeClassName='active'>My Posts</Button>
+        <Button className={styles.link} component={NavLink} exact to={`/post/add`} activeClassName='active'>Add Post</Button>
+        <Button className={styles.link} component={NavLink} exact to={`/`}>Log out</Button>
+      </nav>
+    );
+  }
+
+  userNotAuthorized() {
+    return(
+      <nav>
+        <Button><a href="https://google.com" className={styles.link}>Log in</a></Button>
+      </nav>
     );
   }
 }
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  users: getAllUsers(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Header,
-  // Container as Header,
+  // Component as Header,
+  Container as Header,
   Component as HeaderComponent,
 };
