40ed45bc5365942b6aa617c776d85a5c845003c7
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-27 13:18:43 +0200

    Add usersReducer

---
+++ src/redux/usersRedux.js
@@ -0,0 +1,20 @@
+/* selectors */
+export const getAllUsers = ({users}) => users;
+
+/* action name creator */
+// const reducerName = 'users';
+// const createActionName = name => `app/${reducerName}/${name}`;
+
+/* action types */
+
+/* action creators */
+
+/* thunk creators */
+
+/* reducer */
+export const reducer = (statePart = [], action = {}) => {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+};
